698f6639b4109145a2b1d1159b332413738d5aca
Dhaval572 <dp9718776@gmail.com>
2024-10-02 16:54:14 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,3 +1,4 @@
+// Including user defined library in c++
 #include <iostream>
 #include "demo.cpp"
 using namespace std;
@@ -7,3 +8,16 @@ int main()
     printHello();
     return 0;
 }
+/*
+In c++ you can also make user defined file Which contains some methods such as functions and classes
+
+Here are demo of user defined library (line no. 3) 
+
+Example : #include "demo.cpp" 
+
+you can install demo.cpp file.
+
+to install demo.cpp file click this website within press ctrl key "https://github.com/Dhaval572/very-normal-programming/blob/main/demo.cpp"
+
+I hope you understand that How to make own library to include in your program
+*/

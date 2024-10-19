10a2d26009e44fb3055180bbd9bd96a1983d7c4a
Dhaval572 <dp9718776@gmail.com>
2024-10-19 11:17:33 +0530

    Create digit_count.cpp

---
+++ digit_count.cpp
@@ -0,0 +1,27 @@
+// Count the number of digit
+#include <iostream>
+using namespace std;
+
+void digitCount(int n)
+{
+    int count = 0; 
+    // Loop will run untill n is not equal to 0
+    while (n)
+    {
+        n = n / 10; // Deviding number by 10 until n != 0
+        count++;    // Counting the number of digits
+    }
+
+    cout << count;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+
+    cout << "Number of  digit of (" << n << ") is: ";
+    digitCount(n);
+    return 0;
+}

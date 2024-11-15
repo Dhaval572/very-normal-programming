ae716b6bb8d23d067c822ae29dcea0c8bfc0c69d
Dhaval572 <dp9718776@gmail.com>
2024-11-15 22:50:10 +0530

    Create Heart.cpp

---
+++ Heart.cpp
@@ -0,0 +1,21 @@
+// Draw heart in c++
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "    ******       ******    " << endl;
+    cout << "  **      **   **      **  " << endl;
+    cout << " **        ** **        ** " << endl;
+    cout << "**          ***          **" << endl;
+    cout << "**           *           **" << endl;
+    cout << " **                    **  " << endl;
+    cout << "   **               **   " << endl;
+    cout << "     **           **     " << endl;
+    cout << "       **       **       " << endl;
+    cout << "         **   **         " << endl;
+    cout << "           **           " << endl;
+    
+    cout << "Heart..." << endl;
+    return 0;
+}

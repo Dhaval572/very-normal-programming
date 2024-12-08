9f1cc9562c29a095d72bcf85927d2ea86ac272a2
Dhaval572 <dp9718776@gmail.com>
2024-12-09 00:04:02 +0530

    Create Print_emoji.cpp

---
+++ Print_emoji.cpp
@@ -0,0 +1,16 @@
+// Printing emojies in c++
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    // Set the input and output code pages to UTF-8
+    SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
+
+    // Printing emoji
+    cout << "💜" << endl; 
+
+    return 0;
+}

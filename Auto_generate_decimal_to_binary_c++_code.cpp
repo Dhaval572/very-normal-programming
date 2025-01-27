864de8d5051f4be5b72d976947a8d5e2d5641c7c
Dhaval <dp9718776@gmail.com>
2025-01-27 17:02:32 +0530

    Create Auto_generate_decimal_to_binary_c++_code.cpp

---
+++ Auto_generate_decimal_to_binary_c++_code.cpp
@@ -0,0 +1,76 @@
+// Automatically Generate C++ Code for Decimal to Binary Conversion
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+inline void typingCode(const string &s)
+{
+    for (char c : s)
+    {
+        cout << c;
+        Sleep(100); // Typing speed
+    }
+}
+
+void printIncludes()
+{
+    string firstLine = "#include <iostream>\n";
+    typingCode(firstLine);
+}
+
+void printNamespace()
+{
+    string thirdLine = "using namespace std;\n\n";
+    typingCode(thirdLine);
+}
+
+void printMainFunction()
+{
+    string mainFunc =
+        "int main()\n"
+        "{\n"
+        "    int n;\n"
+        "    cout << \"Enter the number in decimal: \";\n"
+        "    cin >> n;\n\n";
+    typingCode(mainFunc);
+}
+
+void printDecimalToBinaryCode()
+{
+    string decimalToBinaryCode =
+        "    int rem;\n"
+        "    int arr[10];\n"
+        "    int i = 0;\n\n"
+        "    while (n)\n"
+        "    {\n"
+        "        rem = n % 2;\n"
+        "        n = n / 2;\n"
+        "        arr[i] = rem;\n"
+        "        i++;\n"
+        "    }\n\n"
+        "    cout << \"Decimal number converted into Binary: \";\n"
+        "    for (int j = i - 1; j >= 0; j--)\n"
+        "    {\n"
+        "        cout << arr[j];\n"
+        "    }\n\n";
+    typingCode(decimalToBinaryCode);
+}
+
+void printEndMain()
+{
+    string endMain =
+        "    return 0;\n"
+        "}\n";
+    typingCode(endMain);
+}
+
+int main()
+{
+    printIncludes();
+    printNamespace();
+    printMainFunction();
+    printDecimalToBinaryCode();
+    printEndMain();
+
+    return 0;
+}

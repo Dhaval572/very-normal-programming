5d3f0dd177af5f6c88fb77933d48d088e7ef570d
Dhaval <dp9718776@gmail.com>
2025-02-21 23:16:01 +0530

    Create Loding_animation.c

---
+++ Loding_animation.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <windows.h>
+
+void loding()
+{
+	printf("[----------] 0%%");
+	for (int i = 0; i < 10; i++)
+	{
+		Sleep(150);
+		printf("\r[");
+
+		// Will print # for 10 time
+		for (int j = 0; j <= i; j++)
+			printf("#");
+
+		for (int j = i + 1; j < 10; j++)
+			printf("-");
+
+		// (i + 1) * 10 for increase % by 10%
+		printf("] %d%%", (i + 1) * 10);
+	}
+}
+
+void main()
+{
+	loding();
+}

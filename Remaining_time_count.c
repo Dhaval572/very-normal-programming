e925728593b71b4f578843f33ace23c244858948
Dhaval <dp9718776@gmail.com>
2025-02-21 23:55:30 +0530

    Create Remaining_time_count.c

---
+++ Remaining_time_count.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <windows.h>
+
+void countdown(int seconds)
+{
+	while (seconds >= 0)
+	{
+		printf("\rTime remaining: %2d seconds", seconds);
+		Sleep(1000);
+		seconds--;
+	}
+	printf("\rTime's up!                \n");
+}
+
+void main()
+{
+	countdown(10); 
+}

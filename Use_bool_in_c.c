a07ac9f238b989f1a4a6e74aae6d763762715880
Dhaval <dp9718776@gmail.com>
2025-02-22 12:01:15 +0530

    Create Use_bool_in_c.c

---
+++ Use_bool_in_c.c
@@ -0,0 +1,14 @@
+// Use bool in c
+#include <stdio.h>
+#include <stdbool.h>
+
+void main()
+{
+	bool isCodingFun = true;
+    bool isTired = false;
+
+    if (isCodingFun)
+        printf("Yes, coding is fun!\n");
+    if (!isTired)
+        printf("Still coding!\n");
+}
